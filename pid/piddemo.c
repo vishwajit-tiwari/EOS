25ae72857c2cb799a5a2ae2dc2fb015af51cf686
Vishwajit1997 <vishwajitkumar37@gmail.com>
2022-06-07 00:41:49 +0530

    added program for pid & ppid

--- pid/piddemo.c
+++ pid/piddemo.c
@@ -4,6 +4,14 @@
 
 int main(int argc, char const *argv[])
 {
-    
+    pid_t pid;
+    pid_t ppid;
+
+    pid = getpid(); 
+    printf("Process ID pid = %d\n", pid); 
+
+    ppid = getppid();
+    printf("Process Parent ID ppid = %d\n", ppid);
+
     return 0;
 }
