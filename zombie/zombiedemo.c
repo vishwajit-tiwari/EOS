3bbf7ae36cdf8dce885d4ea9b267352996bb2a26
Vishwajit1997 <vishwajitkumar37@gmail.com>
2022-06-12 00:37:42 +0530

    added zombie process

---
+++ zombie/zombiedemo.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    pid_t id;
+
+    printf("Before fork\n");
+    printf("Process: ID: %d PPID: %d\n", getpid(),getppid());
+
+    id = fork();
+
+    printf("After fork\n");
+    
+    if(id == 0){
+        printf("Child process: ID: %d PPID: %d\n", getpid(),getppid());
+
+        printf("Child Completed\n");
+    }
+    else if(id > 0) {
+        printf("Parent process: ID %d PPID: %d\n", getpid(), getppid());
+
+        sleep(10);
+        printf("Parent Completed\n");
+    }
+    else {
+        perror("Error in fork\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    exit(EXIT_SUCCESS);
+    //return 0;
+}
