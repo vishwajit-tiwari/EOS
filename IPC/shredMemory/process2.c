45e94ecc9b5458cc248c85344814df90a2ca132c
Vishwajit1997 <vishwajitkumar37@gmail.com>
2022-08-17 22:48:58 +0530

    Added IPC Machanisum named sharedMemory
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   IPC/shredMemory/Makefile
    	new file:   IPC/shredMemory/process1.c
    	new file:   IPC/shredMemory/process2.c
    
     Untracked files:
    	IPC/messageQueue/

---
+++ IPC/shredMemory/process2.c
@@ -0,0 +1,44 @@
+/**
+ * @file process2.c
+ * @author Vishwajit Kumar Tiwari (you@domain.com)
+ * @brief IPC Machanisum to communicate between two Process
+ * @version 0.1
+ * @date 2022-08-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include<stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <unistd.h>
+#include <sys/types.h>
+
+// Global Variables
+int *count;
+
+
+int main(int argc, char const *argv[])
+{
+    int shmd;
+
+    printf("Inside %s: Function\n", __FUNCTION__);
+
+    // Open shared memory
+    shmd = shm_open("/cdacshm", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+    // Move to predefined length
+    ftruncate(shmd,sizeof(int));
+
+    count = (int*) mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shmd, 0);
+
+    while(1)
+    {
+        printf("Count = %d\n", *count);
+        sleep(1);
+    }
+    
+    return 0;
+}
